f096ef7c459db35af68dc743075a70325d17013a
msansegundo <marinasansegundo@hotmail.com>
2015-05-29 08:25:44 +0200

    Layout y Css

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(partials());
+
 
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -24,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use(partials());
+
 
 //app.use('/users', users);
 
